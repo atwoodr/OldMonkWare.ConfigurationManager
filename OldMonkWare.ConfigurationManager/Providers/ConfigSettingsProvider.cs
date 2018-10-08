74e5094100f166d66a8103640e05aedd636cf984
Ryan Atwood <ryan.atwood@assurant.com>
2018-10-08 15:32:55 -0500

    Add project files.

---
+++ OldMonkWare.ConfigurationManager/Providers/ConfigSettingsProvider.cs
@@ -0,0 +1,53 @@
+﻿using OldMonkWare.Configuration;
+using System.Security;
+
+namespace OldMonkWare.Providers
+{
+    public interface IConfigSettingsProvider
+    {
+        string this[string name] { get; }
+
+        SecureString GetConnectionString(string connectionStringName);
+        SecureString GetProtectedAppSetting(string protectedAppSettingKey);
+        byte[] GetProtectedAppSettingAsByteArray(string protectedAppSettingKey);
+
+        SecureString GetConfigFileSectionSetting(string configFileSettingKey, string configFileSectionName);
+        byte[] GetConfigFileSectionSettingAsByteArray(string configFileSettingKey, string configFileSectionName);
+    }
+
+    public class ConfigSettingsProvider : IConfigSettingsProvider
+    {
+        public string this[string name]
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings[name];
+            }
+        }
+
+        public SecureString GetConnectionString(string connectionStringName)
+        {
+            return SecureConfigurationManager.ReadConnectionString(connectionStringName);
+        }
+
+        public SecureString GetProtectedAppSetting(string protectedAppSettingKey)
+        {
+            return SecureConfigurationManager.ReadProtectedAppSetting(protectedAppSettingKey);
+        }
+
+        public byte[] GetProtectedAppSettingAsByteArray(string protectedAppSettingKey)
+        {
+            return SecureConfigurationManager.ReadProtectedAppSettingAsByteArray(protectedAppSettingKey);
+        }
+
+        public SecureString GetConfigFileSectionSetting(string configFileSettingKey, string configFileSectionName)
+        {
+            return SecureConfigurationManager.ReadConfigFileAttribute(configFileSettingKey, configFileSectionName);
+        }
+
+        public byte[] GetConfigFileSectionSettingAsByteArray(string configFileSettingKey, string configFileSectionName)
+        {
+            return SecureConfigurationManager.ReadConfigFileAttributeAsByteArray(configFileSettingKey, configFileSectionName);
+        }
+    }
+}
---
+++ OldMonkWare.ConfigurationManager/SecureConfigurationManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security;
+
+namespace OldMonkWare.Configuration
+{
+    public static class SecureConfigurationManager
+    {
+        public static SecureString ReadProtectedAppSetting(string protectedAppSettingKey)
+        {
+            SecureString s = new SecureString();
+            s.ReadConfigFileValueAsSecureString(protectedAppSettingKey, "protectedAppSettings");
+            return s;
+        }
+
+        public static SecureString ReadConnectionString(string connectionStringName)
+        {
+            SecureString s = new SecureString();
+            s.ReadConfigFileValueAsSecureString(connectionStringName, "connectionStrings");
+            return s;
+        }
+
+        public static SecureString ReadConfigFileAttribute(string key, string configSectionName)
+        {
+            SecureString s = new SecureString();
+            s.ReadConfigFileValueAsSecureString(key, configSectionName);
+            return s;
+        }
+
+        private static void ReadConfigFileValueAsSecureString(this SecureString value, string key, string configSectionName)
+        {
+            dynamic protectedAppSettings = new Formo.Configuration(configSectionName);
+            char[] appSettingValueCharacters = protectedAppSettings.Get(key)?.ToCharArray() ?? new char[] { };
+
+            for (int i = 0; i < appSettingValueCharacters.Length; i++)
+            {
+                value.AppendChar(appSettingValueCharacters[i]);
+
+                //erase the char values as we go so they are not in memory in the array, awaiting GC
+                appSettingValueCharacters[i] = default(char);
+            }
+
+            value.MakeReadOnly();
+        }
+
+        public static byte[] ReadProtectedAppSettingAsByteArray(string protectedAppSettingKey)
+        {
+            dynamic protectedAppSettings = new Formo.Configuration("protectedAppSettings");
+            return Convert.FromBase64String(protectedAppSettings.Get(protectedAppSettingKey));
+        }
+
+        public static byte[] ReadConfigFileAttributeAsByteArray(string key, string configSectionName)
+        {
+            dynamic protectedAppSettings = new Formo.Configuration(configSectionName);
+            return Convert.FromBase64String(protectedAppSettings.Get(key));
+        }
+
+        /** 
+         * Note: If you get the urge to add a method to this class that returns a string object, think twice:
+         * https://blogs.msdn.microsoft.com/fpintos/2009/06/12/how-to-properly-convert-securestring-to-string/
+         * 
+         * We don't want to encourage getting these protected strings out of our encrypted config files only to put them
+         * into a string which ends up visible to a hacker in the heap.
+         */
+    }
+}
